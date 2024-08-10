c47e686c79edc0be1981237fb5a85f24213210ba
사용자 이름 <@.com>
2024-08-10 19:42:10 +0900

    Add quotes

---
+++ js/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+    {
+        quote: "Insanity is doing the same thing over and over again and expecting different results.",
+        author: "Albert Einstein",
+    },
+
+    {
+        quote: "The important thing is not to stop questioning. Curiosity has its own reason for existing.",
+        author: "Albert Einstein",
+    }
+];
+
+const quote =  document.querySelector("#quote span:first-child");
+const author =  document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length % 2)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
